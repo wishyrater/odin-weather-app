52f3c1589b9c2a55c4c08321abe23370dcbf1efb
Julian Andersen <julian.cesar.andersen@gmail.com>
2025-01-28 22:50:38 +0100

    Add some error messaging details

--- src/GiphyAPIClient.js
+++ src/GiphyAPIClient.js
@@ -7,15 +7,15 @@ const GiphyAPIClient = (() => {
         try {
             const response = await fetch(`${endpoint}?api_key=${apiKey}&s=${searchTerm}`);
             if (!response.ok) {
-                throw new Error(response.status);
+                throw new Error(`HTTP error. Status: ${response.status}`);
             }
             const data = await response.json();
             if (!data.data || data.data.length === 0) {
-                throw new Error('No gifs found');
+                throw new Error('No gifs found for the given search term');
             }
             return data;
         } catch (error) {
-            console.error(error);
+            console.error('Error fetching GIF:', error);
         }
     };
 
